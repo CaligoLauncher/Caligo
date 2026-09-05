f93fb4f594cfa6ec1aa936ff5c854284fcea9c74
WayMee <matviigula@gmail.com>
2026-09-06 00:49:19 +0300

    Rename: Terra Launcher -> Caligo (src/main.rs)

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ fn main() -> eframe::Result {
             .with_inner_size([1000.0, 620.0])
             .with_min_inner_size([720.0, 440.0])
             .with_decorations(false)
-            .with_title("Terra Launcher"),
+            .with_title("Caligo"),
         ..Default::default()
     };
     eframe::run_native(
-        "Terra Launcher",
+        "Caligo",
         options,
-        Box::new(|cc| Ok(Box::new(app::TerraLauncherApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(app::CaligoApp::new(cc)))),
     )
 }
